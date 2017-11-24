bc563899ff1f822e323af138da55529071b63e03
zhaoliu09 <liulianjushi@126.com>
2017-11-24 11:18:13 +0800

    sdk fsd

--- dp/word_break.py
+++ dp/word_break.py
@@ -29,6 +29,8 @@ def word_break(s, word_dict):
     """
     dp = [False] * (len(s)+1)
     dp[0] = True
+
+
     for i in range(1, len(s)+1):
         for j in range(0, i):
             if dp[j] and s[j:i] in word_dict:
